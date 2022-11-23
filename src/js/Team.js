71489072d610b60afac07da3be38e1ee54a08732
bochkarevatat <tanitabt@rambler.ru>
2022-11-23 12:12:49 +0400

    first commit

---
+++ src/js/Team.js
@@ -0,0 +1,32 @@
+/* eslint-disable no-plusplus */
+export default class Team {
+  constructor() {
+    this.characters = new Set();
+  }
+
+  addChar(...heroes) {
+    for (const item of heroes) {
+      this.characters.add(item);
+    }
+  }
+
+  [Symbol.iterator]() {
+    const { members, last } = this;
+    console.log(this);
+    let current = 0;
+    return {
+      next() {
+        if (current < last) {
+          return {
+            done: false,
+            value: members[current++],
+          };
+        }
+        return {
+          done: true,
+          value: undefined,
+        };
+      },
+    };
+  }
+}
---
+++ src/js/app.js
@@ -0,0 +1,13 @@
+import Team from './Team';
+import Bowman from './Bowman';
+import Swordsman from './Swordsman';
+
+const person1 = new Bowman('Лучник');
+const person2 = new Swordsman('Мечник');
+
+const team = new Team();
+team.addChar(person1, person2);
+for (const hero of team) {
+  console.log(hero);
+}
+console.log('add module');
